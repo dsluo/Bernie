43fbd7e249db0502ad6757a942303a1660d03843
dsluo <me@dsluo.dev>
2022-01-29 03:03:27 -0500

    sql fixes

--- src/soundboard/database.rs
+++ src/soundboard/database.rs
@@ -1,8 +1,13 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use serenity::model::id::{GuildId, UserId};
-use sqlx::{Error, FromRow, PgPool, Row};
-use sqlx::postgres::PgRow;
+use sqlx::{
+    Error,
+    FromRow,
+    PgPool,
+    Row,
+    postgres::PgRow
+};
 use url::Url;
 
 use crate::soundboard::{
@@ -24,7 +29,7 @@ impl DatabaseBackend {
     async fn get_sound_id(&self, guild_id: GuildId, name: &str) -> Result<i32> {
         Ok(sqlx::query(
             "select id from sounds \
-            where guild_id = ? and name = ? and deleted_at is null")
+            where guild_id = $1 and name = $2 and deleted_at is null")
             .bind(guild_id.0 as i64)
             .bind(name)
             .map(|row: PgRow| row.get(0))
@@ -43,8 +48,9 @@ impl BackendProvider for DatabaseBackend {
     async fn ensure_guild(&self, guild_id: GuildId) -> Result<Guild> {
         Ok(sqlx::query_as::<_, Guild>(
             "insert into guilds \
-            values(?) \
-            on conflict do nothing \
+            values($1) \
+            on conflict(id) \
+                do update set updated_at = current_timestamp \
             returning *")
             .bind(guild_id.0 as i64)
             .fetch_one(&self.pool)
@@ -56,7 +62,7 @@ impl BackendProvider for DatabaseBackend {
 
         Ok(sqlx::query_as::<_, Sound>(
             "insert into sounds(guild_id, name, source, uploader_id, length) \
-            values(?, ?, ?, ?, ?) \
+            values($1, $2, $3, $4, $5) \
             returning *")
             .bind(guild_id.0 as i64)
             .bind(name)
@@ -70,7 +76,8 @@ impl BackendProvider for DatabaseBackend {
     async fn list(&self, guild_id: GuildId) -> Result<Vec<Sound>> {
         Ok(sqlx::query_as::<_, Sound>(
             "select * from sounds \
-            where guild_id = ? and deleted_at is null")
+            where guild_id = $1 and deleted_at is null \
+            order by name")
             .bind(guild_id.0 as i64)
             .fetch_all(&self.pool)
             .await?)
@@ -78,8 +85,8 @@ impl BackendProvider for DatabaseBackend {
 
     async fn rename(&self, guild_id: GuildId, old_name: &str, new_name: &str) -> Result<Sound> {
         Ok(sqlx::query_as::<_, Sound>(
-            "update sounds set name = ? \
-            where guild_id = ? and name = ? and deleted_at is null \
+            "update sounds set name = $1 \
+            where guild_id = $2 and name = $3 and deleted_at is null \
             returning *")
             .bind(new_name)
             .bind(guild_id.0 as i64)
@@ -91,7 +98,7 @@ impl BackendProvider for DatabaseBackend {
     async fn remove(&self, guild_id: GuildId, name: &str) -> Result<Sound> {
         Ok(sqlx::query_as::<_, Sound>(
             "update sounds set deleted_at = current_timestamp \
-            where guild_id = ? and name = ? and deleted_at is null \
+            where guild_id = $1 and name = $2 and deleted_at is null \
             returning *")
             .bind(guild_id.0 as i64)
             .bind(name)
@@ -102,8 +109,9 @@ impl BackendProvider for DatabaseBackend {
     async fn autocomplete(&self, guild_id: GuildId, starts_with: &str) -> Result<Vec<String>> {
         Ok(sqlx::query(
             "select name from sounds \
-            where guild_id = ? and starts_with(name, ?) and deleted_at is null \
-            limit 25")
+            where guild_id = $1 and starts_with(name, $2) and deleted_at is null \
+            limit 25 \
+            order by name")
             .bind(guild_id.0 as i64)
             .bind(starts_with)
             .map(|row: PgRow| row.get("name"))
@@ -116,7 +124,7 @@ impl BackendProvider for DatabaseBackend {
 
         Ok(sqlx::query_as::<_, Playback>(
             "insert into playbacks(sound_id, player_id) \
-            values(?, ?) \
+            values($1, $2) \
             returning *")
             .bind(sound_id)
             .bind(player_id.0 as i64)
@@ -125,26 +133,6 @@ impl BackendProvider for DatabaseBackend {
     }
 
     async fn random(&self, guild_id: GuildId, player_id: UserId) -> Result<Playback> {
-        // let name = "asdf"; // todo
-        //
-        // let sound_id: i64 = sqlx::query(
-        //     "select id from sounds \
-        //     where guild_id = ? and name = ? and deleted_at is null")
-        //     .bind(guild_id.0 as i64)
-        //     .bind(name)
-        //     .map(|row: PgRow| row.get(0))
-        //     .fetch_one(&self.pool)
-        //     .await?;
-        //
-        // Ok(sqlx::query_as::<_, Playback>(
-        //     "insert into playbacks(sound_id, player_id) \
-        //     values(?, ?) \
-        //     returning *")
-        //     .bind(sound_id)
-        //     .bind(player_id.0 as i64)
-        //     .fetch_one(&self.pool)
-        //     .await?);
-
         todo!()
     }
 
@@ -152,8 +140,8 @@ impl BackendProvider for DatabaseBackend {
         let sound = self.get_sound_id(guild_id, name).await?;
 
         Ok(sqlx::query_as::<_, Playback>(
-            "update playbacks set stopper_id = ?, stopped_at = current_timestamp \
-            where sound_id = ? and stopped_at is null \
+            "update playbacks set stopper_id = $1, stopped_at = current_timestamp \
+            where sound_id = $2 and stopped_at is null \
             returning *")
             .bind(stopper_id.0 as i64)
             .bind(sound)
@@ -167,7 +155,7 @@ impl BackendProvider for DatabaseBackend {
             sqlx::query_as::<_, Playback>(
                 "select playbacks.*, sounds.guild_id from playbacks \
                 inner join sounds on sounds.id = playbacks.sound_id \
-                where guild_id = ? and sound_id = ? \
+                where guild_id = $1 and sound_id = $2 \
                 order by playbacks.created_at desc")
                 .bind(guild_id.0 as i64)
                 .bind(sound_id)
@@ -175,7 +163,7 @@ impl BackendProvider for DatabaseBackend {
             sqlx::query_as::<_, Playback>(
                 "select playbacks.*, sounds.guild_id from playbacks \
                 inner join sounds on sounds.id = playbacks.sound_id \
-                where guild_id = ? \
+                where guild_id = $1 \
                 order by playbacks.created_at desc")
                 .bind(guild_id.0 as i64)
         };
